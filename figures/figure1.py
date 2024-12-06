d228306c0d37c33f087e9441602fe7dec1690c55
Flaviano Della Pia <fd385@icepc02>
2024-12-06 09:26:35 +0000

    Updated figure after revision

---
+++ figures/figure1.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+
+path_to_data='/Users/flavi/Desktop/Cambridge/PhD/Reports/Year-2/1D-ice/analysis_for_paper/data/melting'
+
+
+#data
+
+raman=np.loadtxt(path_to_data+'/exp_raman')
+photo=np.loadtxt(path_to_data+'/exp_photo')
+xrd=np.loadtxt(path_to_data+'/exp_xrd')
+tip4p=np.loadtxt(path_to_data+'/md_tip4p')
+reaxff=np.loadtxt(path_to_data+'/md_reaxff')
+
+# colors
+triangle='#2571ff'
+square='#ff0232'
+penta_1='#ffb927'
+penta_2='#008744'
+hexa='#00ee00'
+
+exp ='black'
+
+
+fig,ax=plt.subplots(figsize=(10,10))
+
+ff1=plt.plot(tip4p[:4,0], tip4p[:4,1],marker='o',ms=10,c='grey',label='MD TIP4P',alpha=0.9,mec='black')
+plt.plot(tip4p[4:11,0], tip4p[4:11,1],marker='o',ms=10,c='grey',alpha=0.9,mec='black')
+plt.plot(tip4p[11:17,0], tip4p[11:17,1],marker='o',ms=10,c='grey',alpha=0.9,mec='black')
+plt.plot(tip4p[17:25,0], tip4p[17:25,1],marker='o',ms=10,c='grey',alpha=0.9,mec='black')
+plt.plot(tip4p[25:,0], tip4p[25:,1],marker='o',ms=10,c='grey',alpha=0.9,mec='black')
+
+ff2=plt.plot(reaxff[:,0], reaxff[:,1],marker='*',ms=12,c='grey',ls='none',label='MD ReaxFF',alpha=0.9,mec='black')
+
+p1=plt.errorbar(4.75*2,287.5,yerr=2.5,fmt='^',ms=14,capsize=10,lw=3.,mec='black',c=triangle,mew=2.)
+p2=plt.errorbar(5.1*2,287.5,yerr=2.5,fmt='s',ms=14,capsize=10,lw=3.,mec='black',c=square,mew=2.)
+p3=plt.errorbar(5.5*2,285,yerr=5,fmt='p',ms=14,capsize=10,lw=3.,mec='black',c=penta_1,mew=2.)
+p4=plt.errorbar(5.9*2,305,yerr=5,fmt='p',ms=14,capsize=10,lw=3.,mec='black',c=penta_2,mew=2.)
+p5=plt.errorbar(6.25*2,295,yerr=5,fmt='h',ms=14,capsize=10,lw=3.,mec='black',c=hexa,mew=2.)
+
+
+eb_raman=plt.errorbar(raman[:,0]*10, raman[:,1]/2.+raman[:,2]/2., yerr=raman[:,2]-raman[:,1],marker='s',ms=10,c=exp,ls='none',mfc='none',capsize=8,label='Raman',alpha=1)
+eb_raman[-1][0].set_linestyle('dotted')
+
+
+
+eb_pl=plt.errorbar(photo[:,0], photo[:,1],yerr= [ ( photo[:,1]-photo[:,2]), (photo[:,3]-photo[:,1])  ], marker='D',ms=10,c=exp,ls='none',mfc='none',label='PL',capsize=8,alpha=1)
+eb_pl[-1][0].set_linestyle('dashed')
+
+eb_xrd=plt.errorbar(xrd[:-1,0], xrd[:-1,1], xerr=(xrd[:-1,3]-xrd[:-1,2])/2., yerr=xrd[:-1,4], marker='>',ms=10,c=exp,ls='none',mfc='none',capsize=8,label='XRD',alpha=1)
+eb_xrd[-1][0].set_linestyle('solid')
+
+
+plt.fill_between(np.arange(8,16,1),270-5,270+5,ls='dashed',color='cornflowerblue',alpha=.2)
+
+
+
+ax.yaxis.set_major_locator(MultipleLocator(20))
+ax.yaxis.set_minor_locator(MultipleLocator(10))
+
+ax.tick_params(axis='both', which='major', length=10)
+ax.tick_params(axis='both', which='minor', length=8)
+
+
+plt.ylim([195,460])
+plt.xticks(np.arange(9.4,14.2,0.8),fontsize=20)
+plt.xlim([9.2,12.65])
+
+ax.tick_params(direction='in',length=8)
+plt.yticks(fontsize=20)
+plt.legend(fontsize=18,shadow=True)
+plt.ylabel('Temperature [K]',fontname='Arial',fontsize=24,labelpad=8)
+plt.xlabel('Diameter [$\mathrm{\AA}$]',fontname='Arial',fontsize=24,labelpad=8)
+#plt.grid(ls='--',alpha=0.4)
+plt.tight_layout()
+
+from matplotlib.legend_handler import HandlerTuple
+import matplotlib.lines as mlines
+tip4p_line = mlines.Line2D([], [], color='grey', marker='o',
+                          markersize=10, label='TIP4P',mec='black',alpha=.9)
+reaxff_line = mlines.Line2D([], [], color='none',markerfacecolor='grey',  marker='*',
+                          markersize=12, label='ReaxFF',mec='black',alpha=.9)
+raman_line = mlines.Line2D([], [], color=exp,mec=exp,markerfacecolor='none',  marker='s',
+                          markersize=10, label='Raman',alpha=1,ls='dotted')
+pl_line = mlines.Line2D([], [], color=exp,mec=exp,markerfacecolor='none',  marker='D',
+                          markersize=10, label='PL',alpha=1,ls='dashed')
+xrd_line = mlines.Line2D([], [], color=exp,mec=exp,markerfacecolor='none',  marker='>',
+                          markersize=10, label='XRD',alpha=1,ls='solid')
+
+
+plt.legend([(p1, p2, p3, p4,p5),tip4p_line,reaxff_line,raman_line,xrd_line,pl_line],['MD MLP','MD TIP4P','MD ReaxFF','Raman','XRD','PL'],scatterpoints=1, numpoints=1, handler_map={tuple: HandlerTuple(ndivide=None)},fontsize=24,shadow=False,edgecolor='black',fancybox=False)
+
+plt.grid(ls='dotted',drawstyle='steps',alpha=.4)
+plt.show()
--- figures/figure_3c.py
+++ figures/figure_3c.py
@@ -1,79 +1,104 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from matplotlib.legend_handler import HandlerTuple
 import matplotlib.pyplot as plt
 import numpy as np
-
-path_to_data='../data/diffusion_coefficient'
-
-d_cnt120=np.loadtxt(path_to_data+'/Dz_cnt120_nvt_vacf')
-d_cnt130=np.loadtxt(path_to_data+'/Dz_cnt130_nvt_vacf')
-d_cnt140=np.loadtxt(path_to_data+'/Dz_cnt140_nvt_vacf')
-d_cnt150=np.loadtxt(path_to_data+'/Dz_cnt150_nvt_vacf')
-d_cnt160=np.loadtxt(path_to_data+'/Dz_cnt160_nvt_vacf')
-
-d_bulk_nvt=np.loadtxt(path_to_data+'/Dz-bulk-nvt')
-
-
 blue='#2571ff'
 red='#ff0232'
 orange='#ffb927'
 green='#008744'
 green2='#00ee00'
 
-#### diffusion vs diameter
+path_to_data='/Users/flavi/Desktop/Cambridge/PhD/Reports/Year-2/1D-ice/analysis_for_paper/data/coexistence_simulations/hbonds'
 
-for i in range(len(d_cnt120)):
-    if(d_cnt120[i,0]==320):
-        d_cnt120_320=[d_cnt120[i,1],d_cnt120[i,2]]
-        
-for i in range(len(d_cnt130)):
-    if(d_cnt130[i,0]==320):
-        d_cnt130_320=[d_cnt130[i,1],d_cnt130[i,2]]
-        
-for i in range(len(d_cnt140)):
-    if(d_cnt140[i,0]==320):
-        d_cnt140_320=[d_cnt140[i,1],d_cnt140[i,2]]
+def  get_error_bootstrap(data):
+    from scipy.stats import bootstrap 
+    data = (data,)  # samples must be in a sequence
+    rng = np.random.default_rng()
+    res = bootstrap(data, np.std, confidence_level=0.9,random_state=rng)
+    return res.confidence_interval[1]
 
-        
-for i in range(len(d_cnt150)):
-    if(d_cnt150[i,0]==320):
-        d_cnt150_320=[d_cnt150[i,1],d_cnt150[i,2]] 
+### load data 
 
-for i in range(len(d_cnt160)):
-    if(d_cnt160[i,0]==320):
-        d_cnt160_320=[d_cnt160[i,1],d_cnt160[i,2]]       
+hb_12_250k=np.loadtxt(path_to_data+'/12,0/HBONDS_NVT_250K.txt')
+hb_13_250k=np.loadtxt(path_to_data+'/13,0/HBONDS_NVT_250K.txt')
+hb_14_250k=np.loadtxt(path_to_data+'/14,0/HBONDS_NVT_250K.txt')
+hb_15_250k=np.loadtxt(path_to_data+'/15,0/HBONDS_NVT_250K.txt')
+hb_16_250k=np.loadtxt(path_to_data+'/16,0/HBONDS_NVT_250K.txt')
+
+hb_12_320k=np.loadtxt(path_to_data+'/12,0/HBONDS_NVT_320K.txt')
+hb_13_320k=np.loadtxt(path_to_data+'/13,0/HBONDS_NVT_320K.txt')
+hb_14_320k=np.loadtxt(path_to_data+'/14,0/HBONDS_NVT_320K.txt')
+hb_15_320k=np.loadtxt(path_to_data+'/15,0/HBONDS_NVT_320K.txt')
+hb_16_320k=np.loadtxt(path_to_data+'/16,0/HBONDS_NVT_320K.txt')
+
+###### average data
+n=1500
+
+av_12_250k=[2*np.mean(hb_12_250k[-n:,1]),2*get_error_bootstrap(hb_12_250k[-n:,1])]
+av_13_250k=[2*np.mean(hb_13_250k[-n:,1]),2*get_error_bootstrap(hb_13_250k[-n:,1])]
+av_14_250k=[2*np.mean(hb_14_250k[-n:,1]),2*get_error_bootstrap(hb_14_250k[-n:,1])]
+av_15_250k=[2*np.mean(hb_15_250k[-n:,1]),2*get_error_bootstrap(hb_15_250k[-n:,1])]
+av_16_250k=[2*np.mean(hb_16_250k[-n:,1]),2*get_error_bootstrap(hb_16_250k[-n:,1])]
+
+data_250=[]
+data_250.append([4.74,av_12_250k[0],av_12_250k[1]])
+data_250.append([5.1,av_13_250k[0],av_13_250k[1]])
+data_250.append([5.54,av_14_250k[0],av_14_250k[1]])
+data_250.append([5.88,av_15_250k[0],av_15_250k[1]])
+data_250.append([6.25,av_16_250k[0],av_16_250k[1]])
+data_250=np.array(data_250)
+
+av_12_320k=[2*np.mean(hb_12_320k[-n:,1]),2*get_error_bootstrap(hb_12_320k[-n:,1])]
+av_13_320k=[2*np.mean(hb_13_320k[-n:,1]),2*get_error_bootstrap(hb_13_320k[-n:,1])]
+av_14_320k=[2*np.mean(hb_14_320k[-n:,1]),2*get_error_bootstrap(hb_14_320k[-n:,1])]
+av_15_320k=[2*np.mean(hb_15_320k[-n:,1]),2*get_error_bootstrap(hb_15_320k[-n:,1])]
+av_16_320k=[2*np.mean(hb_16_320k[-n:,1]),2*get_error_bootstrap(hb_16_320k[-n:,1])]
+
+data_320=[]
+data_320.append([4.74,av_12_320k[0],av_12_320k[1]])
+data_320.append([5.1,av_13_320k[0],av_13_320k[1]])
+data_320.append([5.54,av_14_320k[0],av_14_320k[1]])
+data_320.append([5.88,av_15_320k[0],av_15_320k[1]])
+data_320.append([6.25,av_16_320k[0],av_16_320k[1]])
+data_320=np.array(data_320)
 
 
+fig,ax= plt.subplots(figsize=(7,7))
 
-diff_320=[]
-diff_320.append([4.75*2,d_cnt120_320[0]])
-diff_320.append([5.1*2,d_cnt130_320[0]])
-diff_320.append([5.5*2,d_cnt140_320[0]])
-diff_320.append([5.9*2,d_cnt150_320[0]])
-diff_320.append([6.25*2,d_cnt160_320[0]])
-diff_320=np.array(diff_320)
+ax.errorbar(data_320[:,0]*2,data_320[:,1],2*data_320[:,2],ls='--',lw=1.5,label='300K',c='black')
+
+p1=ax.plot(data_320[0,0]*2,data_320[0,1],marker='^',lw=1.5,ms=16,c='black',mfc=blue,mec='black',mew=2.)
+p2=ax.plot(data_320[1,0]*2,data_320[1,1],marker='s',lw=1.5,ms=16,c='black',mfc=red,mec='black',mew=2.)
+p3=ax.plot(data_320[2,0]*2,data_320[2,1],marker='p',lw=1.5,ms=16,c='black',mfc=orange,mec='black',mew=2.)
+p4=ax.plot(data_320[3,0]*2,data_320[3,1],marker='p',lw=1.5,ms=16,c='black',mfc=green,mec='black',mew=2.)
+p5=ax.plot(data_320[4,0]*2,data_320[4,1],marker='h',lw=1.5,ms=16,c='black',mfc=green2,mec='black',mew=2.)
 
 
-fig, ax = plt.subplots(figsize=(7,7))
+ax.errorbar(data_250[:,0]*2,data_250[:,1],2*data_250[:,2],ls='--',lw=1.5,label='250K',c='grey')
+p6=ax.plot(data_250[0,0]*2,data_250[0,1],marker='^',lw=1.5,ms=16,c='grey',mew=1.5,mec='black')
+p7=ax.plot(data_250[1,0]*2,data_250[1,1],marker='s',lw=1.5,ms=16,c='grey',mew=1.5,mec='black')
+p8=ax.plot(data_250[2,0]*2,data_250[2,1],marker='p',lw=1.5,ms=16,c='grey',mew=1.5,mec='black')
+p9=ax.plot(data_250[3,0]*2,data_250[3,1],marker='p',lw=1.5,ms=16,c='grey',mew=1.5,mec='black')
+p10=ax.plot(data_250[4,0]*2,data_250[4,1],marker='h',lw=1.5,ms=16,c='grey',mew=1.5,mec='black')
 
-plt.errorbar(x=4.75*2,y=d_cnt120_320[0]*10.**(9),yerr=2*d_cnt120_320[1]*10.**(9),fmt='^',ms=18,mfc=blue,mec='black',c='black',mew=2)
-plt.errorbar(x=5.1*2,y=d_cnt130_320[0]*10.**(9),yerr=2*d_cnt130_320[1]*10.**(9),fmt='s',ms=18,mfc=red,mec='black',c='black',mew=2)
-plt.errorbar(x=5.5*2,y=d_cnt140_320[0]*10.**(9),yerr=2*d_cnt140_320[1]*10.**(9),fmt='p',ms=18,mfc=orange,mec='black',c='black',mew=2)
-plt.errorbar(x=5.9*2,y=d_cnt150_320[0]*10.**(9),yerr=2*d_cnt150_320[1]*10.**(9),fmt='p',ms=18,mfc=green,mec='black',c='black',mew=2)
-plt.errorbar(x=6.25*2,y=d_cnt160_320[0]*10.**(9),yerr=2*d_cnt160_320[1]*10.**(9),fmt='h',ms=18,mfc=green2,mec='black',c='black',label='Ice (6,0)',mew=2)
 
-plt.plot(diff_320[:,0],diff_320[:,1]*10.**(9),c='black',ls='--',lw=1.5,label='320K')
+plt.legend(
+    [(p1[0], p2[0], p3[0], p4[0], p5[0]), (p6[0], p7[0], p8[0], p9[0], p10[0])],  # Make sure each element is an actual plot element
+    ['320 K', '250 K'],
+    handler_map={tuple: HandlerTuple(ndivide=None)},  # Map tuples to HandlerTuple
+    scatterpoints=1, numpoints=1, fontsize=24, shadow=False,
+    edgecolor='black', fancybox=False
+)
 
-plt.axhline(d_bulk_nvt[-1:,1]*10.**(9),lw=2,c='grey',ls='dashed')
 
-plt.xlim([4.7*2,6.3*2])
-plt.xticks(np.arange(9.4,12.8,0.4),fontsize=28,rotation=45)
+plt.xlim([4.7*2,6.30*2])
 plt.yticks(fontsize=28)
-ax.yaxis.get_offset_text().set_fontsize(20)
 ax.tick_params(length=8)
-plt.ylabel(r'$D_z$ [$10^{-9} \mathrm{m^2 s^{-1}}$]',fontsize=30,fontname='arial',labelpad=8)
-plt.xlabel(r'Diameter [$\mathrm{\AA}$]',fontsize=30,fontname='arial',labelpad=8)
+plt.xticks(np.arange(9.4,13.0,0.4),fontsize=28,rotation=45)
+plt.ylabel('H-Bonds per water',fontsize=30,fontname='arial',labelpad=8)
+plt.xlabel('Diameter [$\mathrm{\AA}$]',fontsize=30,fontname='arial',labelpad=6)
 plt.grid(ls='dotted',drawstyle='steps',alpha=.4)
 plt.tight_layout()
 plt.show()
